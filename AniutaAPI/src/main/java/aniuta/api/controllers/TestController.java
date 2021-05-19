237ec308d090ba3d52154b40b2077fbd9f843b9c
HoSyHuy <huy.ho@rikai.technology>
2021-05-19 10:48:46 +0700

    huyhs

--- AniutaAPI/src/main/java/aniuta/api/controllers/TestController.java
+++ AniutaAPI/src/main/java/aniuta/api/controllers/TestController.java
@@ -16,7 +16,7 @@ public class TestController {
 		return "Public Content.";
 	}
 	
-	@GetMapping("/user")
+	@GetMapping("/users")
 	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
 	public String userAccess() {
 		return "User Content.";
